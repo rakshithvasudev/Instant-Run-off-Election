2695b8303968a0d0bd69cbe4e6218557f0599003
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-04-17 23:43:34 -0700

    party test added

--- tst/PartyTest.java
+++ tst/PartyTest.java
@@ -1,5 +1,34 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by Rakshith on 4/10/2017.
  */
 public class PartyTest {
+
+    @Test
+    public void usageTest(){
+
+        //Given (Arrange)
+        Party party1;
+        //When(Action)
+        party1=Party.TEA;
+
+        // Then(Assert)
+        Assert.assertTrue(Party.valueOf("TEA").equals(party1));
+    }
+
+    @Test
+    public void usageTest2(){
+
+        //Given (Arrange)
+        Party party1;
+        //When(Action)
+        party1=Party.UKK;
+
+        // Then(Assert)
+        Assert.assertTrue(Party.valueOf("UKK").equals(party1));
+    }
+
+
 }
