5f9ac4c8afd7d93f69b7a9998f3d8d1e2494e68e
Rakshith Vasudev <rakshithvbharani@gmail.com>
2017-04-17 23:46:15 -0700

    added vote test

--- tst/VoteTest.java
+++ tst/VoteTest.java
@@ -1,5 +1,21 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by Rakshith on 4/10/2017.
  */
 public class VoteTest {
+
+    @Test
+    public void constructorTest(){
+
+        //Given (Arrange)
+        String[] preferences ={"Ronald","Hermy", "Henry"};
+        Vote vote;
+        //When(Action)
+        vote = new Vote(preferences);
+
+        // Then(Assert)
+        Assert.assertTrue(vote.getPreferences().size()>=0);
+    }
 }
